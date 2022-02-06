692000ef9b1294e6d093e15b7cf794e49f45ae54
stargvzer <58994802+stargvzer@users.noreply.github.com>
2022-02-06 18:30:30 +0300

    Update kolchok.py

--- kolchok.py
+++ kolchok.py
@@ -2,9 +2,13 @@ import random
 
 token = "5257043486:AAEne2vsG3AXfOJ8jyO1w9INKUb2jUQFxx4"
 
-startAnswer = 'Прколятый срущий ньюсрустер....'
+startAnswer = 'Проклятый срущий ньюсрустер....'
 
 f = open("dict.txt","r")
 lines = f.readlines()
 
 random_message = lambda: random.choice(lines)
+
+
+
+
