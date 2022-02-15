aad1153de23899e4da92bec077356d87500c3343
stargvzer <58994802+stargvzer@users.noreply.github.com>
2022-02-16 02:02:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import telebot
-import telegram
-
 import kolchok
 
 
 bot = telebot.TeleBot(kolchok.token)
 
 
-@bot.message_handler(commands=['1'])
-def handle_start(messages):
-    bot.send_message(messages.chat.id, kolchok.random_message())
-
-
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    bot.send_message(message.chat.id, kolchok.random_message())
 
 
 bot.infinity_polling()
