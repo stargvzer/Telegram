cb6cc98ace78212ee7f92912a6a33fad449aa728
stargvzer <58994802+stargvzer@users.noreply.github.com>
2022-02-16 04:11:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ bot = telebot.TeleBot(kolchok.token)
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.send_message(message.chat.id, kolchok.startAnswer)
-    
+
+
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     bot.send_message(message.chat.id, kolchok.random_message())
-    
-    bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
