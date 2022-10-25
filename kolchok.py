217e3a3a566d9f1f57fcfbf8ceb2fc092f9b9449
stargvzer <58994802+stargvzer@users.noreply.github.com>
2022-10-25 12:46:10 +0300

    Update kolchok.py

--- kolchok.py
+++ kolchok.py
@@ -3,9 +3,6 @@ from telegram.ext import Updater
 
 
 token = "5257043486:AAEne2vsG3AXfOJ8jyO1w9INKUb2jUQFxx4"
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
 
 f = open("dict.txt","r")
 lines = f.readlines()
