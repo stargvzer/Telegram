6aaa23bd3c9f93dc3b9e52053ff0389c0f4c85ca
stargvzer <58994802+stargvzer@users.noreply.github.com>
2022-10-25 12:56:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ bot = telebot.TeleBot(kolchok.token)
 
 def start(update, context):
 bot.send_message(message.chat.id, kolchok.random_message())
-
 start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
  
 if __name__ == '__main__': 
     bot.polling()
