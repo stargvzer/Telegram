ab8c2b37c7c47174cceb1a6b34d63b54336493dd
stargvzer <58994802+stargvzer@users.noreply.github.com>
2022-10-25 12:51:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ bot.send_message(message.chat.id, kolchok.random_message())
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-updater.start_polling()
-
  
 if __name__ == '__main__': 
     bot.polling()
