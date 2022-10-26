5fe0da6fc2b0a50f3c1cbcabe328b293b715582f
stargvzer <58994802+stargvzer@users.noreply.github.com>
2022-10-26 21:41:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ def handle_text(messages):
     bot.send_message(messages.chat.id, kolchok.random_message())
 
 if __name__ == "__main__":
-bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
